f472199eb1d349b414eefddf15a6ebb38ac0f2ad
GDB <gdb.thirteen@gmail.com>
2020-03-20 19:41:08 -0400

    Product.java updated

--- src/music/models/Product.java
+++ src/music/models/Product.java
@@ -6,33 +6,44 @@ import java.io.Serializable;
 public class Product implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    private String code;
+    private String description;
+    private double price;
+    
+    
     public Product() {}
 
     public Product(String code, String description, double price) {
+    	this.code = code;
+    	this.description = description;
+    	this.price = price;
     }
 
     public void setCode(String code) {
+    	this.code = code;
     }
 
     public String getCode() {
-        return null;
+        return this.code;
     }
 
     public void setDescription(String description) {
+    	this.description = description;
     }
 
     public String getDescription() {
-        return null;
+        return this.description;
     }
 
     public void setPrice(double price) {
+    	this.price = price;
     }
 
     public double getPrice() {
-        return 0;
+        return this.price;
     }
 
     public String getPriceCurrencyFormat() {
-        return null;
+        return String.valueOf(this.price);
     }
 }

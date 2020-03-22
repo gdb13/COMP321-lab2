5219714aab2c8b329325c7683af9df3f4a45fee1
GDB <gdb.thirteen@gmail.com>
2020-03-22 14:23:13 -0400

    created CartServlet

--- src/controller/CartServlet.java
+++ src/controller/CartServlet.java
@@ -1,7 +1,6 @@
 package controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -11,23 +10,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import music.data.ProductIO;
 import music.models.Product;
 
 /**
- * Servlet implementation class HomeServlet
+ * Servlet implementation class CartServlet
  */
-
-@WebServlet("/")
-public class HomeServlet extends HttpServlet {
+@WebServlet("/Cart")
+public class CartServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public HomeServlet() {
+    public CartServlet() {
         super();
-        
+        // TODO Auto-generated constructor stub
     }
 
 	/**
@@ -36,19 +33,18 @@ public class HomeServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		HttpSession session = request.getSession();
-		List<Product> products = new ArrayList<>();
-		session.setAttribute("cart", products);
+		List<Product> products = (List<Product>) session.getAttribute("cart");
 		
-		getServletContext().getRequestDispatcher("/WEB-INF/jsp/index.jsp").forward(request, response);
+		request.setAttribute("products", products);
+		getServletContext().getRequestDispatcher("/WEB-INF/jsp/cart.jsp").forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		// TODO Auto-generated method stub
 		doGet(request, response);
-
 	}
 
 }
